8ab4152f455c0b85784e79be4668f6e7549b7780
Nikhil Dange <nsdange@ucsd.edu>
2023-09-17 23:05:22 -0700

    set up supabase

--- order-service/src/api/order.ts
+++ order-service/src/api/order.ts
@@ -1,16 +1,27 @@
 import express, { Request, Response, NextFunction } from 'express';
+import config from '../config';
+import { createClient } from '@supabase/supabase-js'
+
 
 const router = express.Router();
 
+// Create a single supabase client for interacting with database
+const supabase = createClient(config.supabaseUrl,config.supabaseKey);
+
 
 // Placing an order
-router.post('/order', (req: Request, res: Response, next: NextFunction) => {
+router.post('/order', async (req: Request, res: Response, next: NextFunction) => {
+
+    
    res.json({ error: null, message: "Hi mom" });
 });
 
 // Getting all orders
-router.get('/order', (req: Request, res: Response, next: NextFunction) => {    
-    res.json({ error: null, message: "Hi mom" });
+router.get('/order', async (req: Request, res: Response, next: NextFunction) => {    
+    const {data, error} = await supabase
+    .from('orders')
+    .select()
+    res.send(data);
 });
 
 // Getting a specific order
--- order-service/src/config/index.ts
+++ order-service/src/config/index.ts
@@ -6,6 +6,8 @@ dotenv.config();
 const config = {
     port: process.env.PORT,
     amqpUrl: process.env.AMQP_URL as string,
+    supabaseUrl: process.env.SUPABASE_URL as string,
+    supabaseKey: process.env.SUPABASE_KEY as string,
   };
   
   export default config;
